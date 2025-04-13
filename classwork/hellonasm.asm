d55db61ce4503db95e6ee2e0429bddff68bc88bf
sarmadalikhanofficial <sarmadali074@gmail.com>
2025-04-14 02:45:58 +0500

    div added

--- classwork/hellonasm.asm
+++ classwork/hellonasm.asm
@@ -1,15 +1,8 @@
-; ORG 100h IS LIBRARY
-org 100h
-
-; variable declaration
-mov ah, 09h
-; variable for printing msg
-mov dx, msg
-; for printing variable
-int 21h
-; one more variable 
-mov ax, 4C00h
-int 21h
-; save to database
+org 100h ; ORG 100h IS LIBRARY
+mov ah, 09h ; variable declaration
+mov dx, msg ; variable for printing msg
+int 21h ; for printing variable
+mov ax, 4C00h ; one more variable 
+int 21h ; save to database
 msg db 'Hello from NASM$', 0
 ; 'Hello from NASM$'terminator
--- classwork/mul.asm
+++ classwork/mul.asm
@@ -1,6 +1,6 @@
 ORG 0X100
-MOV AL, 3      ; First number in AL (lower 8 bits of AX)
-MOV BL, 4      ; Second number in BL (lower 8 bits of BX)
-MUL BL         ; Multiply AL by BL, result in AX
+MOV AL, 3 ; First number in AL (lower 8 bits of AX)
+MOV BL, 4 ; Second number in BL (lower 8 bits of BX)
+MUL BL    ; Multiply AL by BL, result in AX
 MOV AH, 4CH    ; Exit function in AH
-INT 21H        ; DOS interrupt
+INT 21H   ; DOS interrupt
