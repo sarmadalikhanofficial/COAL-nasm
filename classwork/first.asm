dc92ee4be673076177a8436ab75ea90b7f6d198e
sarmadalikhanofficial <sarmadali074@gmail.com>
2025-04-13 16:58:44 +0500

    classwork separate folder added

---
+++ classwork/first.asm
@@ -0,0 +1,15 @@
+; ORG 100h IS LIBRARY
+org 100h
+
+; variable declaration
+mov ah, 09h
+; variable for printing msg
+mov dx, msg
+; for printing variable
+int 21h
+; one more variable 
+mov ax, 4C00h
+int 21h
+; save to database
+msg db 'Hello from NASM$', 0
+; 'Hello from NASM$'terminator
